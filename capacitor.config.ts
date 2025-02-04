8f19b461a93d5b68e80fb76f030e3050be2acbbd
Noah Davidson <njdavidson0@gmail.com>
2025-02-03 19:12:40 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Lift',
+  webDir: 'dist'
+};
+
+export default config;
